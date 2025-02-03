e44e5fc11dfe9c8ca64f67dd572225c3a4911882
ELKA-IBRAHIM <elkassimiibrahim@gmail.com>
2025-02-03 23:08:30 +0100

    Finally works ......

--- 211/Robot_v0.py
+++ 211/Robot_v0.py
@@ -61,17 +61,23 @@ class Robot_in_Room:
     
     def s_g(self, s):
             sensors  = self.sensors
-            i = np.where(sensors == s)[0][0]
-            s = self.sensor_glob_frame()[i] #Tous les capteurs!!!
-            return s
+            i = 0
+            while i < len(sensors):
+                if sensors[i][0] == s[0] and sensors[i][1] == s[1] and sensors[i][2] == s[2]:
+                    break  
+                else:
+                    i+=1    
+                
+            sensor = self.sensor_glob_frame()[i]    
+            return sensor
     # Check whether sensor on right side of wall
 
     def visible(self,s,wall):
-        s_g = self.s_g(s) #Tous les capteurs!!!
+        s_g = self.s_g(s) #global frame coordinates
+        
         xs = s_g[0]
         ys = s_g[1]
-        
-        
+
         t = [wall[0][0]-wall[1][0], wall[0][1]-wall[1][1]]
         n = [t[1], -t[0]]
 
@@ -84,23 +90,42 @@ class Robot_in_Room:
                 
     
     # Get Sensor Readings
+    def distance(self, s, wall):
+        sg = self.s_g(s)
+        xs, ys, theta = sg[0], sg[1], sg[2] 
+
+        xa = wall[0][0]
+        ya = wall[0][1]
+
+        xb = wall[1][0]
+        yb = wall[1][1]
+
+        A = np.array([[np.cos(theta), xa-xb], [np.sin(theta), ya-yb]])
+        b = np.array([[xa-xs], [ya-ys]])
+
+        try:
+            ts = np.linalg.solve(A, b)  # Résolution du système
+            t, s = ts
+            
+            if 0 <= s <= 1 and t > 0:  # Vérifier si l'intersection est sur le segment
+                return t  # Distance suivant le vecteur
+        except np.linalg.LinAlgError:
+            pass  # Le système ne peut pas être résolu (vecteurs colinéaires)
+    
+
     def get_sensor_readings(self):
         # simulated distances
-        ym = np.zeros(self.nb_sensors)
-
-        d = {}
-        for i in range(1,self.nb_sensors+1):
-            d[i] = []
-            s = self.sensor_glob_frame()[i]
-            xs, ys, theta = s[0], s[1], s[3]
+        sensors = self.sensors
+        ym = []
+        for s in sensors:
+            d_s = [] # liste des distances entre le capteur et les murs dans la direction du capteur 
             for wall in self.walls:
-                if self.visible(s, wall):
+                d = self.distance(s, wall)
+                d_s.append(d)
+            ym.append(min(filter(lambda x: x is not None, d_s)))
+        
+        return np.array(ym).flatten()
 
-
-
-
-        # A compléter
-        return ym
         
     # Plots
     def plot(self):
@@ -120,30 +145,14 @@ class Robot_in_Room:
 
       
 
-walls = np.array([[[0,8],[0,0]],
-                          [[6,8],[0,8]],
-                          [[6,6],[6,8]],
-                          [[10,6],[6,6]],
-                          [[10,0],[10,6]],
-                          [[0,0],[10,0]],
-                          [[6,2],[5,4]],
-                          [[5,4],[7,5]],
-                          [[7,5],[8,3]],
-                          [[8,3],[6,2]]])
 
 
-
-p_star = np.array([2,2,0.8])     
+p_star = np.array([8,1,0.8])     
     
 myproblem = Robot_in_Room(p_star)
 
 ym = myproblem.get_sensor_readings()        
 myproblem.plot()
             
-#print('Simulated distances : ',ym)
+print('Simulated distances : ',ym)
 
-s = np.array([0.5,0.25,np.pi/4])
-print(myproblem.sensor_glob_frame())
-for wall in walls:
-    print(myproblem.visible(s,wall))
-    
